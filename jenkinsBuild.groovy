4a69c02a1baf6f45346f2aa1fadd6e39880b46ff
EC2 Default User <ec2-user@ip-172-31-33-90.ec2.internal>
2020-07-22 23:54:31 +0000

    characters fixed

--- jenkinsBuild.groovy
+++ jenkinsBuild.groovy
@@ -40,9 +40,9 @@ def slavePodTemplate = """
 
     if (branch == "master") {
       environment = "prod"
-    } else if (branch.contains('dev-feature/')) {
+    } else if (branch.contains("dev-feature/")) {
       environment = "dev"
-    } else if (branch.contains('qa-feature/')) {
+    } else if (branch.contains("qa-feature/")) {
       environment = "qa"
     }
 
