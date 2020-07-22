c2c2e3c61c7daaf53d4f90fa91d43c08f7a42acb
EC2 Default User <ec2-user@ip-172-31-33-90.ec2.internal>
2020-07-22 23:33:20 +0000

    if script environment added

--- jenkinsBuild.groovy
+++ jenkinsBuild.groovy
@@ -35,7 +35,12 @@ def slavePodTemplate = """
             hostPath:
               path: /var/run/docker.sock
     """
+    def environment = ""
     def branch          = "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase()
+
+    if (branch == master ) {
+       environment = "prod"
+    }
 
     podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
       node(k8slabel) {
@@ -60,7 +65,7 @@ def slavePodTemplate = """
                   build job: 'artemis-deploy', 
                   parameters: [
                       [$class: 'BooleanParameterValue', name: 'terraformApply',     value: true],
-                      [$class: 'StringParameterValue',  name: 'environment',         value: "dev"]
+                      [$class: 'StringParameterValue',  name: 'environment',         value: "${environment}"]
                       ]
                 }
             }
