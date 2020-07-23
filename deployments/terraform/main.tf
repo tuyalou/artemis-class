99dd2680b978d1a3529c198ce01db193910b9502
tuyalou <63755131+tuyalou@users.noreply.github.com>
2020-07-22 20:44:50 -0500

    Create main.tf

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,22 @@
+module "artemis-deploy" {
+  source  = "fuchicorp/chart/helm"
+
+  deployment_name        = "artemis"
+  deployment_environment = "${var.deployment_environment}"
+  deployment_endpoint    = "none" 
+  deployment_path        = "artemis"
+
+  template_custom_vars  = {     
+    deployment_image     = "${var.deployment_image}"    
+  }
+}
+
+
+variable "deployment_environment" {
+  default = "dev"
+}
+
+
+variable "deployment_image" {
+  default = "fsadykov/artemis:master"
+}
