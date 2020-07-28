5e6f5a7d9e18c40bbdb58822081824847de697c5
EC2 Default User <ec2-user@ip-172-31-33-90.ec2.internal>
2020-07-28 00:20:52 +0000

    kube config fixed

--- jenkinsDeploy.groovy
+++ jenkinsDeploy.groovy
@@ -75,7 +75,6 @@ def slavePodTemplate = """
             """.stripIndent()
             writeFile file: 'deployment_configuration.tfvars', text: "${deployment_configuration_tfvars}" 
             sh 'cat deployment_configuration.tfvars >> dev.tfvars'
-            sh 'sh /scripts/Dockerfile/set-config.sh'
           
           }   
         }
